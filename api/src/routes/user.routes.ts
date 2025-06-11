0a3199b4a11c897d778710ed5d8cee0886e83b60
vishranti09 <vishrantidange09@gmail.com>
2025-06-11 20:24:19 +0530

    axios setup

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -12,14 +12,14 @@ router.patch('/updateMe', userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 // Rutas de administrador
-router.use(restrictTo('admin'));
-router.route('/')
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
+// router.use(restrictTo('admin'));
+// router.route('/')
+//   .get(userController.getAllUsers)
+//   .post(userController.createUser);
 
-router.route('/:id')
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+// router.route('/:id')
+//   .get(userController.getUser)
+//   .patch(userController.updateUser)
+//   .delete(userController.deleteUser);
 
 export default router;
